8cfbe9eae393afa5772843a3d4ce2003858b58a9
Biao Wang <biaow@fnal.gov>
2025-07-28 14:56:07 -0500

    Create startFarBB.sh

---
+++ crbin/startFarBB.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# This script starts the SNEWS forwarder backbone.
+# This starts the SNEWSMessage Forwarder on novadaq-far-gateway-01
+# so we can receive messages from BNL and then forwards them
+# novadaq-near-trigger as well as the relevant receivers
+#
+# This file is /home/novadaq/DAQOperationsTools/bin/startSNEWSMessageBackBone.sh
+#
+
